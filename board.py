768e7570873971632f955a48dc90b7814deb784f
Joseph Kim <joseph.kim.kr@gmail.com>
2017-11-13 11:04:21 -0800

    refactor and bug fix to scramble method

--- board.py
+++ board.py
@@ -1,6 +1,7 @@
 """Define the Board and Cell classes."""
 
 # from random import randint
+from copy import deepcopy
 from random import choice
 
 
@@ -9,7 +10,7 @@ class PracticeBoard(object):
 
     def __init__(self, state, open_cell_coords, size):
         """Constructor for the PracticeBoard class."""
-        self.practice_state = state
+        self.practice_state = deepcopy(state)
         self.practice_open_cell_coords = open_cell_coords
         self.practice_size = size
 
@@ -19,9 +20,10 @@ class PracticeBoard(object):
 
     def practice_slide(self, coords):
         """Slide the tile at the given coordinates into the open cell."""
-        tile_to_move = self._practice_tile_at_coords(coords)
-        self.practice_state[coords[1] - 1][coords[0] - 1] = self.practice_size ** 2
-        self.practice_state[self.practice_open_cell_coords[1] - 1][self.practice_open_cell_coords[0] - 1] = tile_to_move
+        # tile_to_move = self._practice_tile_at_coords(coords)
+        # self.practice_state[coords[1] - 1][coords[0] - 1] = self.practice_size ** 2
+        # self.practice_state[self.practice_open_cell_coords[1] - 1][self.practice_open_cell_coords[0] - 1] = tile_to_move
+        self.practice_state[self.practice_open_cell_coords[1] - 1][self.practice_open_cell_coords[0] - 1], self.practice_state[coords[1] - 1][coords[0] - 1] = self.practice_state[coords[1] - 1][coords[0] - 1], self.practice_state[self.practice_open_cell_coords[1] - 1][self.practice_open_cell_coords[0] - 1]
         self.practice_open_cell_coords = coords
 
         return self.practice_state
@@ -48,6 +50,7 @@ class Board(object):
                 num += 1
 
             self.state.append(row)
+        self.previous_states.append(deepcopy(self.state))
 
     def _tile_at_coords(self, coords):
         """Take in 1-indexed x-y coords and return the tile number there."""
@@ -57,7 +60,7 @@ class Board(object):
 
     def slide(self, coords):
         """Slide the tile at the given coordinates into the open cell."""
-        print('before', self.state)
+        # print('before', self.state)
         self.previous_states.append(self.state)
         self.state[self.open_cell_coords[1] - 1][self.open_cell_coords[0] - 1], self.state[coords[1] - 1][coords[0] - 1] = self.state[coords[1] - 1][coords[0] - 1], self.state[self.open_cell_coords[1] - 1][self.open_cell_coords[0] - 1]
         self.open_cell_coords = coords
@@ -87,32 +90,39 @@ class Board(object):
 
     def _make_random_move(self):
         """Make a random move. Moves which would result in a previous state are disallowed."""
-        check_board = PracticeBoard(self.state, self.open_cell_coords, self.size)
+        invalid_move = True
+        while invalid_move:
+            check_board = PracticeBoard(self.state, self.open_cell_coords, self.size)
 
-        # potential_move = self.legal_moves[randint(0, len(self.legal_moves) - 1)]
+            # potential_move = self.legal_moves[randint(0, len(self.legal_moves) - 1)]
 
-        potential_move = choice(self.legal_moves)
+            potential_move = choice(self.legal_moves)
 
-        if check_board.practice_slide(potential_move) in self.previous_states:
-            self.legal_moves.remove(potential_move)
-            self._make_random_move()
+            if check_board.practice_slide(potential_move) in self.previous_states:
+                self.legal_moves.remove(potential_move)
+                # self._make_random_move()
+            else:
+                invalid_move = False
 
         else:
             self.slide(potential_move)
+            self.previous_states.append(deepcopy(self.state))
 
 
 def generate_board_states(num_of_moves, attempts, size=3):
     """From a solved board, make a number of random moves, and save unique states."""
     for i in range(attempts):
-        states = []
+        # states = []
         board = Board(size)
+        # import pdb; pdb.set_trace()
 
         for j in range(num_of_moves):
             board._make_random_move()
 
-            if board.state not in states:
-                states.append(board.state)
+            # if board.state not in states:
+            #     states.append(board.state)
 
+        print()
 
 if __name__ == "__main__":
-    generate_board_states(1, 5)
+    generate_board_states(30, 5)
