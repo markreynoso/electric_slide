17ca49b23dc00523ab424fb1ab8b2b56c9f670e4
Nathan Moore <ncmoore1986@gmail.com>
2017-11-13 09:09:23 -0800

    changes to board prototype

--- board.py
+++ board.py
@@ -3,44 +3,27 @@
 from random import randint
 
 
-class Cell(object):
-    """Make an instance of the Cell class."""
-
-    def __init__(self, num, coords):
-        """Constructor for the Cell class."""
-        self.tile = num
-        self.coords = coords
-
-    def __repr__(self):
-        """Show the tile value rather than the object."""
-        return str(self.tile)
-
-
 class PracticeBoard(object):
     """Make an instance of the PracticeBoard class."""
 
     def __init__(self, state, open_cell_coords, size):
         """Constructor for the PracticeBoard class."""
-        self.state = state
-        self.open_cell_coords = open_cell_coords
-        self.size = size
+        self.practice_state = state
+        self.practice_open_cell_coords = open_cell_coords
+        self.practice_size = size
 
-    def _tile_number_at_coords(self, coords):
-        """Take in 1-indexed x-y coordinates and return the tile number at that location."""
-        return self.state[coords[1] - 1][coords[0] - 1].tile
+    def _practice_tile_at_coords(self, coords):
+        """Take in 1-indexed x-y coords and return the tile number there."""
+        return self.practice_state[coords[1] - 1][coords[0] - 1]
 
-    def _cell_at_coords(self, coords):
-        """Take in 1-indexed x-y coordinates and return the Cell object at that location."""
-        return self.state[coords[1] - 1][coords[0] - 1]
-
-    def slide(self, coords):
+    def practice_slide(self, coords):
         """Slide the tile at the given coordinates into the open cell."""
-        tile_to_move = self._tile_number_at_coords(coords)
-        self._cell_at_coords(coords).tile = self.size ** 2
-        self._cell_at_coords(self.open_cell_coords).tile = tile_to_move
-        self.open_cell_coords = coords
+        tile_to_move = self._practice_tile_at_coords(coords)
+        self.practice_state[coords[1] - 1][coords[0] - 1] = self.practice_size ** 2
+        self.practice_state[self.practice_open_cell_coords[1] - 1][self.practice_open_cell_coords[0] - 1] = tile_to_move
+        self.practice_open_cell_coords = coords
 
-        return self.state
+        return self.practice_state
 
 
 class Board(object):
@@ -55,32 +38,31 @@ class Board(object):
         self.legal_moves = [(size - 1, size), (size, size - 1)]
 
         num = 1
-        y_coord = 1
+
         for i in range(size):
-            x_coord = 1
             row = []
+
             for j in range(size):
-                row.append(Cell(num, (x_coord, y_coord)))
+                row.append(num)
                 num += 1
-                x_coord += 1
-            y_coord += 1
+
             self.state.append(row)
 
-    def _tile_number_at_coords(self, coords):
-        """Take in 1-indexed x-y coordinates and return the tile number at that location."""
-        return self.state[coords[1] - 1][coords[0] - 1].tile
-
-    def _cell_at_coords(self, coords):
-        """Take in 1-indexed x-y coordinates and return the Cell object at that location."""
-        return self.state[coords[1] - 1][coords[0] - 1]
+    def _tile_at_coords(self, coords):
+        """Take in 1-indexed x-y coords and return the tile number there."""
+        x = coords[0] - 1
+        y = coords[1] - 1
+        return self.state[y][x]
 
     def slide(self, coords):
         """Slide the tile at the given coordinates into the open cell."""
+        print(self.state)
         self.previous_states.append(self.state)
-        tile_to_move = self._tile_number_at_coords(coords)
-        self._cell_at_coords(coords).tile = self.size ** 2
-        self._cell_at_coords(self.open_cell_coords).tile = tile_to_move
+        tile_to_move = self._tile_at_coords(coords)
+        self.state[self.open_cell_coords[1] - 1][self.open_cell_coords[0] - 1] = tile_to_move
+        self.state[coords[1] - 1][coords[0] - 1] = self.size ** 2
         self.open_cell_coords = coords
+        print(self.state)
 
         self._determine_legal_moves(coords)
 
@@ -110,7 +92,7 @@ class Board(object):
 
         potential_move = self.legal_moves[randint(0, len(self.legal_moves) - 1)]
 
-        if check_board.slide(potential_move) in self.previous_states:
+        if check_board.practice_slide(potential_move) in self.previous_states:
             self.legal_moves.remove(potential_move)
             self._make_random_move()
 
@@ -119,21 +101,17 @@ class Board(object):
 
 
 def generate_board_states(num_of_moves, attempts, size=3):
-    """From a solved board, make a given number of random moves, and save unique board states."""
-    states = []
-
+    """From a solved board, make a number of random moves, and save unique states."""
     for i in range(attempts):
+        states = []
         board = Board(size)
 
         for j in range(num_of_moves):
             board._make_random_move()
 
-        if board.state not in states:
-            states.append(board.state)
-
-    for state in states:
-        print(state)
+            if board.state not in states:
+                states.append(board.state)
 
 
 if __name__ == "__main__":
-    generate_board_states(1, 10)
+    generate_board_states(1, 5)
