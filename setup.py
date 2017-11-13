55ba938e08ff23bc5d893d899660ad11f4a9a698
Mark Reynoso <mreynoso@spu.edu>
2017-11-13 09:47:11 -0800

    some html modification to bootstrap

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 
 setup(
     name="electric_slide",
-    description="Command-line script used to track donations and \
-    write thank you notes.",
+    description="Machine learning project to solve slider puzzles.",
     py_modules=['board'],
     install_requires=[
 
