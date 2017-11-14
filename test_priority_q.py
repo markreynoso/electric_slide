1f1448f3456f57f998368c2142cc41d36b1e5757
Megan Flood <mak.flood@comcast.net>
2017-11-14 15:51:15 -0800

    Tests for the PriorityQ Added tests for all the methods of the PriorityQ class. !00% coverage with all tests passing.

--- test_priority_q.py
+++ test_priority_q.py
@@ -6,32 +6,6 @@ import pytest
 START_STATE = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 
-@pytest.fixture
-def n0():
-    """A starting Node."""
-    from priority_q import Node
-    return Node(START_STATE, None, None)
-
-
-@pytest.fixture
-def n1(n0):
-    """A child Node after n0."""
-    from priority_q import Node
-    next_state = [[1, 2, 3], [4, 5, 6], [7, 9, 8]]
-    return Node(next_state, (2, 3), n0)
-
-
-@pytest.fixture
-def chain7(n0, n1):
-    """A chain of 5 nodes."""
-    from priority_q import Node
-    n2 = Node([[1, 2, 3], [4, 5, 6], [9, 7, 8]], (1, 3), n1)
-    n3 = Node([[1, 2, 3], [9, 5, 6], [4, 7, 8]], (1, 2), n2)
-    n4 = Node([[9, 2, 3], [1, 5, 6], [4, 7, 8]], (1, 1), n3)
-    n5 = Node([[2, 9, 3], [1, 5, 6], [4, 7, 8]], (2, 1), n4)
-    return Node([[2, 5, 3], [1, 9, 6], [4, 7, 8]], (2, 2), n5)
-
-
 def test_construct_a_new_start_node():
     """Test that a starting node can be constructed."""
     from priority_q import Node
@@ -127,3 +101,120 @@ def test_find_all_legal_moves_for_open_side(n1):
 def test_find_all_legal_moves_for_open_center(chain7):
     """Test that all legal moves are found for an open center."""
     assert sorted(chain7.legal_moves()) == sorted([(1, 2), (2, 1), (3, 2), (2, 3)])
+
+
+def test_constructing_a_priority_q_is_empty():
+    """Test that a new PriorityQ is empty."""
+    from priority_q import PriorityQ
+    q = PriorityQ()
+    assert q.values == {}
+
+
+def test_pushing_new_prioity_value_adds_to_queue(empty_queue):
+    """Test that a pushing a new priority into a queue adds the value."""
+    q = empty_queue
+    q.push('a', 1)
+    assert q.values == {1: ['a']}
+
+
+def test_pushing_value_same_priority_adds_to_queue(empty_queue):
+    """Test that a pushing a new value to the same priority adds the value."""
+    q = empty_queue
+    q.push('a', 1)
+    q.push('b', 1)
+    assert q.values == {1: ['a', 'b']}
+
+
+def test_pushing_different_priorities_adds_to_queue(empty_queue):
+    """Test that a pushing a diff priorities adds both values."""
+    q = empty_queue
+    q.push('a', 1)
+    q.push('b', 2)
+    assert q.values == {1: ['a'], 2: ['b']}
+
+
+def test_pushing_different_many_values_to_priorities_adds_to_queue(empty_queue):
+    """Test that a pushing values to diff priorities adds all values."""
+    q = empty_queue
+    q.push('a', 1)
+    q.push('b', 2)
+    q.push('c', 1)
+    assert q.values == {1: ['a', 'c'], 2: ['b']}
+
+
+def test_popping_from_empty_queue_raises_error(empty_queue):
+    """Test that popping from empty queue raises ValueError."""
+    with pytest.raises(ValueError):
+        empty_queue.pop()
+
+
+def test_popping_only_item_from_queue_empties_it(empty_queue):
+    """Test that poppingonly item from queue empties it."""
+    empty_queue.push('a', 2)
+    empty_queue.pop()
+    assert empty_queue.values == {}
+
+
+def test_popping_value_returns_lowest_priority(filled_queue):
+    """Test that popping values from queue removes lowest priority."""
+    assert filled_queue.pop() == 'e'
+
+
+def test_popping_only_value_from_priority_removes_priority(filled_queue):
+    """Test that popping only value from priority removes priority."""
+    filled_queue.pop()
+    assert 0 not in filled_queue.values
+
+
+def test_popping_value_from_queue_does_not_remove_priority(filled_queue):
+    """Test that popping value from multi value priority."""
+    filled_queue.pop()
+    assert filled_queue.pop() == 'd'
+    assert 1 in filled_queue.values
+
+
+def test_found_priority_of_value_in_queue_is_correct(filled_queue):
+    """Test that the priority found is correct."""
+    assert filled_queue.priority('c') == 4
+
+
+def test_found_priority_of_value_not_in_queue_is_none(filled_queue):
+    """Test that the priority found of value not in queue is None."""
+    assert filled_queue.priority('x') is None
+
+
+def test_removing_value_without_priority_removes_from_queue(filled_queue):
+    """Test that removing from the queue without giving a priority works."""
+    filled_queue.remove('d')
+    assert filled_queue.priority('d') is None
+
+
+def test_removing_only_value_with_priority_removes_priority(filled_queue):
+    """Test that removing only value from priority remove priority."""
+    filled_queue.remove('f', 6)
+    assert filled_queue.priority('f') is None
+    assert 6 not in filled_queue.values
+
+
+def test_removing_value_with_priority_removes_from_queue(filled_queue):
+    """Test that removing from the queue with a priority works."""
+    filled_queue.remove('d', 1)
+    assert filled_queue.priority('d') is None
+
+
+def test_removing_value_not_in_queue_gets_error(filled_queue):
+    """Test that removing item not in queue raises a KeyError."""
+    with pytest.raises(KeyError):
+        filled_queue.remove('x')
+
+
+def test_removing_value_with_incorrect_priority_gets_error(filled_queue):
+    """Test that removing item with incorrect priority raises ValueError."""
+    with pytest.raises(ValueError):
+        filled_queue.remove('d', 6)
+
+
+def test_removing_value_with_priority_not_in_queue_gets_error(filled_queue):
+    """Test that removing item with invalid priority raises KeyError."""
+    with pytest.raises(KeyError):
+        filled_queue.remove('d', 9)
