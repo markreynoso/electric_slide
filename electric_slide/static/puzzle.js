f853be62431d4111857a9bd628bf1f0e95415b13
Megan Flood <mak.flood@comcast.net>
2017-11-15 18:14:46 -0800

    Board now starts at solved state.

--- electric_slide/static/puzzle.js
+++ electric_slide/static/puzzle.js
@@ -79,7 +79,7 @@ function solveBoard(interval) {
 }
 
 // startBoard(startingState);
-startBoard([[6, 4, 7], [8, 5, 9], [3, 2, 1]]);
+startBoard([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
 
 
 $('#shuffle-button').on('click', function() {
